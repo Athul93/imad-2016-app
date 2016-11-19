d832d5c51996f853566ce62076af1162a2c4fc12
Athul93 <athulb01@gmail.com>
2016-11-19 12:37:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,13 @@ app.get('/logout', function (req, res) {
 });
 
 var pool = new Pool(config);
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
